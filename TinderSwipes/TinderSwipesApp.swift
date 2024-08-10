4aaa14b976ab4d7a33eaca4504cbfa3926616be1
HaoYun <haoyun0317@gmail.com>
2024-08-10 17:07:41 +0800

    Initial Commit

---
+++ TinderSwipes/TinderSwipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderSwipesApp.swift
+//  TinderSwipes
+//
+//  Created by  謝皓昀 on 2024/8/10.
+//
+
+import SwiftUI
+
+@main
+struct TinderSwipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
